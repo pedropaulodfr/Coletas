f846cfe14904f9438ddec0074d96d916c56d8bfd
Pedro Paulo <pedropaulodfr@outlook.com>
2023-04-25 21:25:07 -0300

    Ajuste rotas privadas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import Modulos from './pages/Modulos/Modulos'
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Modulos from './pages/Modulos/Modulos';
 import PrivateRoutes from './utils/PrivateRoutes';
 
-
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route elements={<PrivateRoutes />} >
-            <Route exact path="/" component={Home} />
-            <Route path="/modulos" component={Modulos} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/modulos" element={<Modulos />} />
           </Route>
-          <Route path="/login"><Login /></Route>
         </Routes>
       </Router>
-
     </div>
   );
 }
